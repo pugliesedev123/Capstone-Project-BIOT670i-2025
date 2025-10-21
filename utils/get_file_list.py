362c8d23473f5961194df07e35082c9e852e768e
rrrrrojas <lisa.rojas.29@gmail.com>
2025-10-21 13:45:39 -0400

    updated get_file_list.py
    Refactor file path handling for cross-platform compatibility and improve comments.

--- utils/get_file_list.py
+++ utils/get_file_list.py
@@ -1,54 +1,73 @@
 import os, re
 
 print("This script will output a csv file with two columns: a directory column and a file name column")
+print("This script will traverse down 3 levels i.e. Top folder > Subfolder (owner) > Sub-subfolder (taxon) to get the contents of the sub-subfolder")
 print("It will also remove empty taxon folders from the existing owner\\* subfolders")
 
-#For Mac
-#user_path = "data/train"
-#pattern = re.compile(r"[\s\S]*/data/train/")
 
+if os.name = "posix":
+    #For Mac
+    user_path = "data/train"
+    pattern = re.compile(r"[\s\S]*/data/train/")
+    pattern2 = "/"
 
-#For Windows
-#user_path = "data\\train"
-#pattern = re.compile(r"[\s\S]*\\data\\train\\")
+else:
+    #For Windows
+    user_path = "data\\train"
+    pattern = re.compile(r"[\s\S]*\\data\\train\\")
+    pattern2 = "\\"
 
 
 output = "Directory_File_output.csv"
 
-#Just paste the file path
-user_path = input("Paste path of directory to get contents")
+#Comment out the above and uncomment the below to run as user decided
+#paste the file path
+#user_path = input("Paste path of directory to get contents")
+'''
+if os.name = "posix":
+    pattern2 = "/"
+else:
+    pattern2 = "\\"
+
+pattern = user_path[:user_path.rfind(pattern2)+1]
+'''
 
 
+#Change to user path 
 os.chdir(user_path)
 
+
+#Keep output file open throughout directory search
 with open(output, "w") as o:
+    #Loop through contents of the Top Folder (user_path)
     for folder in os.listdir():
+        #Move into subfolder if directory
         if os.path.isdir(folder):
             os.chdir(folder)
             #print(f"in {folder}")
+            #Loop through contents of subfolder
             for f2 in os.listdir():
+                #Move into sub-subfolder f2 if directory
                 if os.path.isdir(f2):
                     os.chdir(f2)
-                    #print(f"in {f2}")
+                    #Check if folder is empty. If empty, delete folder.
                     if len(os.listdir()) == 0:
                         print(f"Remove {f2} from data/{folder}!")
                         os.chdir("..")
                         os.rmdir(f2)
                         continue
-                    fullpath = os.path.abspath(folder)
-                    remove = re.search(pattern,fullpath).group()
-                    #print(fullpath)
-                    fullpath = fullpath.replace(remove, "")
-                    #print(fullpath)
-                    directory = fullpath[:fullpath.rfind("/")]
+                    #Process the filepath text in order to get the owner/taxon file path only
+                    directory = f"{folder}{pattern2}{f2}"
+                    #Get all of the files in the current taxon folder
                     for file in os.listdir(): 
-                        #DS_Store conditional was because my macbook has these weird hidden files that keep popping up
+                        #DS_Store conditional was because macbook has these weird hidden files that keep popping up
                         if os.path.isfile(file) and "DS_Store" not in file:
-                            #print(file)
+                            #Write the directory and file to the output file
                             o.write(directory + "," + file + "\n")
 
-                        
+                    #After finishing reading files in taxon folder, move back up to the owner folder
                     os.chdir("..")
+            #After finishing the taxon folders in an owner folder, move back up to top folder
             os.chdir("..")
 
 print("Script Completed")
